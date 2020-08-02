4414b535290afeffe1b53eee881156cc38c9d929
George Talnagi <gtalnagi@yahoo.com>
2020-08-02 19:50:33 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  grace_hopper = programmer_hash[:grace_hopper]
+    grace_hopper
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  alan_kay = programmer_hash[:alan_kay][:known_for]
+    alan_kay
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     dennis_ritchie = programmer_hash[:dennis_ritchie][:languages]
+     dennis_ritchie[0]
+     
 end
 
 def adding_matz
@@ -79,7 +85,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:yukihiro_matsumoto] = [:known_for] << "Ruby", [:languages] << ["LISP", "C"]
+    programmer_hash
     
 end
 
@@ -99,10 +106,12 @@ def changing_alan
           :languages => ["C"]
         }
      }
+     
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
