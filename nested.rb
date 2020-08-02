4c1b2e6080cf65f71e99916d52961b5ec4268cff
George Talnagi <gtalnagi@yahoo.com>
2020-08-02 19:16:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
         }
      }
      dennis_ritchie = programmer_hash[:dennis_ritchie][:languages]
-     dennis_ritchie
+     dennis_ritchie.split
 end
 
 def adding_matz
